289d3b94fc592f0372a90c2777f343add34e946f
Mirek Kędzierski <miroslaw.kedzierski@acoustic.co>
2024-05-29 12:30:43 +0200

    feat: add basic layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,63 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import * as Table from '$lib/components/ui/table';
+	import { daysOfTheWeek } from '$lib/data/days';
+	const forcasts = [
+		{
+			id: 'INV001',
+			city: 'Oslo',
+			temp: [26, 19, 21, 22],
+			icon: '/weathericons/sleet.svg'
+		},
+		{
+			id: 'INV002',
+			city: 'Bergen',
+			temp: [23, 20, 21, 22],
+			icon: '/weathericons/clearsky_day.svg'
+		},
+		{
+			id: 'INV003',
+			city: 'Stavanger',
+			temp: [24, 20, 21, 22],
+			icon: '/weathericons/cloudy.svg'
+		},
+		{
+			id: 'INV004',
+			city: 'Trondheim',
+			temp: [16, 20, 21, 22],
+			icon: '/weathericons/clearsky_day.svg'
+		},
+		{
+			id: 'INV005',
+			city: 'Trondheim',
+			temp: [22, 20, 21, 22],
+			icon: '/weathericons/clearsky_day.svg'
+		}
+	];
+</script>
+
+<Table.Root>
+	<Table.Caption></Table.Caption>
+	<Table.Header>
+		<Table.Row>
+			<Table.Head class="w-[200px]">Invoice</Table.Head>
+			<Table.Head>{daysOfTheWeek[2]}</Table.Head>
+			<Table.Head>{daysOfTheWeek[3]}</Table.Head>
+			<Table.Head>{daysOfTheWeek[4]}</Table.Head>
+			<Table.Head>{daysOfTheWeek[5]}</Table.Head>
+		</Table.Row>
+	</Table.Header>
+	<Table.Body>
+		{#each forcasts as forcast, i (i)}
+			<Table.Row>
+				<Table.Cell class="font-medium"><a href="/forecast/city/">{forcast.city}</a></Table.Cell>
+				{#each forcast.temp as temp, j}
+					<Table.Cell
+						><div class="flex items-center">
+							<img src={forcast.icon} alt="" class="w-9" />{forcast.temp[0]}{j}
+						</div></Table.Cell
+					>
+				{/each}
+			</Table.Row>
+		{/each}
+	</Table.Body>
+</Table.Root>
---
+++ src/routes/forecast/[city]/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import * as Table from '$lib/components/ui/table';
+	import { cityForcast } from '$lib/data/cityforcast';
+	import type { Main, DayInterval, TwentyFourHourSymbol } from '$lib/data/cityforcast.d.js';
+
+	const dayIntervals: Array<DayInterval> = cityForcast.dayIntervals;
+  //TODO: ustawić zmienne globalne - może na tłumacznie
+	const national = 'no-NO';
+
+	const getFormattedDate = (dateTime) => {
+    console.log(dateTime);
+		const date = new Date(dateTime);
+		// // const options = {dateStyle: 'full'};
+    const options = { weekday: 'long', month: 'long', day: 'numeric' };
+		const formattedDate = date.toLocaleDateString(national, options);
+    return formattedDate;
+	};
+</script>
+
+<Table.Root>
+	<Table.Header>
+		<Table.Row>
+			<Table.Head></Table.Head>
+			<Table.Head>Night</Table.Head>
+			<Table.Head>Morning</Table.Head>
+			<Table.Head>Afternoon</Table.Head>
+			<Table.Head>Evening</Table.Head>
+			<Table.Head>Temperature</Table.Head>
+			<Table.Head>Precip</Table.Head>
+			<Table.Head>wind</Table.Head>
+			<Table.Head></Table.Head>
+		</Table.Row>
+	</Table.Header>
+	<Table.Body>
+		{#each dayIntervals as dayInterval, i}
+			<Table.Row>
+				<Table.Cell><strong>{getFormattedDate(dayInterval.start)}</strong></Table.Cell>
+        {#each dayInterval.sixHourSymbols as symbol, i}
+          <Table.Cell>
+            {#if symbol}
+              <img src={`/weathericons/${symbol}.svg`} alt="" class="w-9" />
+            {/if}
+          </Table.Cell>
+        {/each}
+				<Table.Cell>{dayInterval.temperature.min}/{dayInterval.temperature.max}</Table.Cell>
+				<Table.Cell>{dayInterval.precipitation.value}/mm</Table.Cell>
+				<Table.Cell>{dayInterval.wind.max}</Table.Cell>
+				<Table.Cell>Dodać dialog</Table.Cell>
+			</Table.Row>
+		{/each}
+	</Table.Body>
+</Table.Root>
+
+<!-- {#each dayIntervale as day, i}
+	<div class="flex items-stretch gap-2">
+		{#each day.sixHourSymbols as symbol, i}
+			<div>{symbol}</div>
+		{/each}
+	</div>
+	<hr />
+{/each} -->
